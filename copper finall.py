5549c21a7dcc09ae4d80cdca18037804496833f1
Mariya stephen <132537481+MariyaStephen2002@users.noreply.github.com>
2023-11-21 22:20:43 +0530

    copper finall.py

--- copper finall.py
+++ copper finall.py
@@ -1,115 +1,130 @@
-# Importing Libraries
-import pickle
 import streamlit as st
 import pandas as pd
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+from sklearn.model_selection import train_test_split
 import numpy as np
-from PIL import Image
 
-# Setting Webpage Configurations
-st.set_page_config(page_icon="⚙",page_title="Copper modeling", layout="wide")
+# Load the data
+@st.cache
+def load_data():
+    df = pd.read_csv("C:\\Users\\stephen\\Downloads\\Copper_final.csv")
+    df.drop(['Unnamed: 0'], axis=1, inplace=True)
+    return df
 
+# Train models
+@st.cache(allow_output_mutation=True)
+def train_models(df):
+    x = df.drop(['selling_price'], axis=1)
+    y = df['selling_price']
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20)
+    
+    model_price = RandomForestRegressor(n_estimators=40)
+    model_price.fit(x_train, y_train)
 
+    a = df.drop(['status'], axis=1)
+    b = df['status']
+    a_train, a_test, b_train, b_test = train_test_split(a, b, test_size=0.20)
+    
+    model_status = RandomForestClassifier(n_estimators=10)
+    model_status.fit(a_train, b_train)
 
-# Loading the model
-st.cache_data
-def load_model():
-    model = pickle.load(open('E:\\visual studio\\copper\\price_prediction.pkl','rb'))
-    return model
+    return model_price, model_status
 
-st.cache_data
-def load_encoder():
-    encoder = pickle.load(open('E:\\visual studio\\copper\\encoder.pkl','rb'))
-    return encoder
+# Main code
+main_bg = "rgb(240, 240, 240)"
+main_fg = "#000000"
 
-st.cache_data
-def load_class_model():
-    model = pickle.load(open('E:\\visual studio\\copper\\Status_prediction.pkl','rb'))
-    return model
+# Set page config
+st.set_page_config(
+    page_title="Industrial Copper Modelling",
+    page_icon=None,
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
 
-# Price prediction model
-reg_model = load_model()
+# Apply styling
+st.markdown(
+    f"""
+    <style>
+        .reportview-container {{
+            background-color: {main_bg};
+            color: {main_fg};
+        }}
+        .sidebar .sidebar-content {{
+            background-color: {main_bg};
+        }}
+        .streamlit-button {{
+            color: {main_fg};
+            background-color: #4CAF50;
+        }}
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
 
-# encoder
-encoder = load_encoder()
+# Load data
+df = load_data()
 
-# Status Prediction model
-class_model = load_class_model()
+# Train models
+model_price, model_status = train_models(df)
 
-# Reading the Dataframe
-model_df = pd.read_csv(r'E:\visual studio\copper\Copper.csv')
+# Sidebar with tabs
+selected_tab = st.sidebar.radio("Select a tab", ["Copper Price Prediction", "Status Prediction"])
 
-# Querying Win/Lost status
-query_df = model_df.query("status == 'Won' or status == 'Lost'")
+# Main content
+if selected_tab == "Copper Price Prediction":
+    # Copper Price Prediction
+    st.header("Copper Price Prediction")
 
-tab1,tab2 = st.tabs(['Selling Price Prediction','Status Prediction'])
+    # User input for copper price prediction
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        customer = st.selectbox("Customer ID", df["customer"].unique())
+        thickness = st.number_input("Thickness of Copper", min_value=0.0)
+        product_ref = st.selectbox("Product Reference", df["product_ref"].unique())
+    with col2:
+        country = st.selectbox("Country Code", df["country"].unique())
+        quantity_tons = st.number_input("Quantity in Tons", min_value=0.0)
+        status = st.radio("Status", ("Won", "Lost"))
+        nday = st.number_input("Expected Number of Days for Delivery", min_value=0)
+    with col3:
+        application = st.selectbox("Application Code", df["application"].unique())
+        width = st.number_input("Width of Copper", min_value=0.0)
+        item_type = st.selectbox("Item Type", df["item type"].unique())
 
-with tab1:
-
-    item_year = st.selectbox('Select the Item year',options = query_df['item_date'].value_counts().index.sort_values())
-
-    country = st.selectbox('Select the Country Code',options = query_df['country'].value_counts().index.sort_values())
-
-    item_type = st.selectbox('Select the Item type',options = query_df['item type'].unique())
-
-    application = st.selectbox('Select the Application number',options = query_df['application'].value_counts().index.sort_values())
-
-    product_ref = st.selectbox('Select the Product Category',options = query_df['product_ref'].value_counts().index.sort_values())
-
-    delivery_year = st.selectbox('Select the Delivery year',options = query_df['delivery date'].value_counts().index.sort_values())
-
-    thickness = st.number_input('Enter the Thickness')
-    log_thickness = np.log(thickness)
-
-    width = st.number_input('Enter the width')
-    log_width = np.log(width)
-
-    quantity_tons = st.number_input('Enter the Quantity (in tons)')
-    log_quantity = np.log(quantity_tons)
-
-    submit = st.button('Predict Price')
-
+    # Prediction button
+    submit = st.button("Predict Copper Price")
     if submit:
-    
-        user_input = pd.DataFrame([[item_year,country,item_type,application,log_thickness,log_width,product_ref,delivery_year,quantity_tons]],
-                            columns = ['item_date','country','item type','application','thickness','width','product_ref','delivery date','quantity_tons'])
-        
-        prediction = reg_model.predict(user_input)
-        
-        selling_price = np.exp(prediction)
-        st.subheader(f':green[Predicted Price] : {round(selling_price[0])}')
+        data = np.array([[quantity_tons, customer, country, 1.0 if status == "Won" else 0.0, item_type, application, thickness, width, product_ref, nday]])
+        pred_r = model_price.predict(data)
+        st.success(f"The predicted selling price is: {pred_r[0]}")
 
-with tab2:
+if selected_tab == "Status Prediction":
+    # Status Prediction
+    st.header("Status Prediction")
 
-    country = st.selectbox('Select any one Country Code',options = query_df['country'].value_counts().index.sort_values())
+    # User input for status prediction
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        customer_ = st.selectbox("Customer ID", df["customer"].unique())
+        thickness_ = st.number_input("Thickness of Copper", min_value=0.0)
+        product_ref_ = st.selectbox("Product Reference", df["product_ref"].unique())
+    with col2:
+        country_ = st.selectbox("Country Code", df["country"].unique())
+        quantity_tons_ = st.number_input("Quantity in Tons", min_value=0.0)
+        selling_price_ = st.number_input("Selling Price of Copper", min_value=0.0)
+        nday_ = st.number_input("Expected Number of Days for Delivery", min_value=0)
+    with col3:
+        application_ = st.selectbox("Application Code", df["application"].unique())
+        width_ = st.number_input("Width of Copper", min_value=0.0)
+        item_type_ = st.selectbox("Item Type", df["item type"].unique())
 
-    item_type = st.selectbox('Select any one Item type',options = query_df['item type'].unique())
-
-    product_ref = st.selectbox('Select any one Product Category',options = query_df['product_ref'].value_counts().index.sort_values())
-
-    delivery_year = st.selectbox('Select a Delivery year',options = query_df['delivery date'].value_counts().index.sort_values())
-
-    thickness = st.number_input('Enter an Thickness')
-    log_thickness = np.log(thickness)
-
-    width = st.number_input('Enter an width')
-    log_width = np.log(width)
-
-    selling_price = st.number_input('Enter an Selling Price')
-    log_selling_price = np.log(selling_price)
-
-    quantity_tons = st.number_input('Enter an Quantity (in tons)')
-    log_quantity = np.log(quantity_tons)
-
-    user_input_1 = pd.DataFrame([[country,item_type,log_thickness,log_width,product_ref,delivery_year,log_selling_price,log_quantity]],
-                       columns = ['country','item type','thickness','width','product_ref','delivery date','selling_price','quantity_tons'])
-    
-    submit1 = st.button('Predict')
-
-    if submit1:
-        transformed_data = encoder.transform(user_input_1)
-        prediction = class_model.predict(transformed_data)
-        
-        if prediction[0] == 1:
-            st.subheader(':green[Predicted Status] : Won')
-        else:
-            st.subheader('green[Predicted Status] : Lost')
+    # Prediction button
+    submit_ = st.button("Predict Status")
+    if submit_:
+        data1 = np.array([[quantity_tons_, customer_, country_, item_type_, application_, thickness_, width_, product_ref_, selling_price_, nday_]])
+        pred_c = model_status.predict(data1)
+        status_message = "The status is Won" if pred_c[0] == 1 else "The status is Lost"
+        st.success(status_message)
+ 
+ # streamlit run "e:/visual studio/Copper vi.py"
